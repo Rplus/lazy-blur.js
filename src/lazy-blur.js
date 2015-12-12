66720baa784581097d9551d871a54df9660cc55d
Rplus <rplus.tw@gmail.com>
2015-12-13 03:11:52 +0800

    add SVG blur filter

--- src/lazy-blur.js
+++ src/lazy-blur.js
@@ -17,6 +17,14 @@ class LazyBlur {
    *         className of imgL
    *         default: 'lazy-blur__imgL'
    *
+   * @param  {String}       filterSelector
+   *         css selector for SVG filter
+   *         default: `html.svg *:not(.done) > ${opt.imgSQuery}`
+   *
+   * @param  {Number}       blurSize
+   *         value of svg gaussian blur filter
+   *         default: 20
+   *
    * @param  {Function}     callback
    *         after imgL loaded
    *         default: `addClass('done')` for imgS' parent
@@ -26,8 +34,10 @@ class LazyBlur {
     opt = Object.assign({
       imgSQuery: '.lazy-blur__imgS',
       imgLClass: 'lazy-blur__imgL',
+      filterSelector: 'html.svg *:not(.done) > ' + opt.imgSQuery,
       getSrc: (imgS) => { return imgS.getAttribute('data-src'); },
       callback: (imgS) => { imgS.parentElement.className += ' done '; },
+      blurSize: 20,
       event: 'click'
     }, opt);
 
@@ -39,6 +49,28 @@ class LazyBlur {
     if (typeof opt.callback !== 'function') {
       opt.callback = false;
     }
+
+    // append svg filter
+    let inlineSvg = `
+    <svg xmlns="http://www.w3.org/2000/svg" style="position: absolute; width: 1px; height: 1px; padding: 0; margin: -1px; overflow: hidden; clip: rect(0, 0, 0, 0); border: 0;">
+      <def>
+        <filter id="lazy-blur-filter">
+          <feGaussianBlur stdDeviation="${opt.blurSize}"></feGaussianBlur>
+        </filter>
+      </def>
+    </svg>
+    <style>
+      ${opt.filterSelector} {
+        -webkit-filter: url("#lazy-blur-filter");
+                filter: url("#lazy-blur-filter");
+      }
+    </style>`;
+
+    document.body.appendChild((() => {
+      let newEl = document.createElement('div');
+      newEl.innerHTML = inlineSvg;
+      return newEl;
+    })());
 
     let appendSrcImg = (imgS) => {
       let imgL = new Image();
